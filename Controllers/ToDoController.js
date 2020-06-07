e79b1b5ff8f2e59efed5f86d61f677ea762b4aaf
PanthThakkar <30841919+PanthThakkar@users.noreply.github.com>
2020-06-07 19:17:30 -0400

    Making sure that Posting and Deleting a task works

--- Controllers/ToDoController.js
+++ Controllers/ToDoController.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var data = [{item: 'Get Milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
@@ -8,12 +9,14 @@ module.exports = function(app){
         response.render('todo', {todos: data});
     });
     app.post('/todo', urlEncodedParser, function(request, response){
-        console.log(request.body, " ", data);
         data.push(request.body);
-        response.render('todo', {todos: data});
-        response.json({todos: data});
+        response.json(data);
+
     });
-    app.delete('/todo:item', function(request, response){
-        
+    app.delete('/todo/:item', function(request, response){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== request.params.item;
+        });
+        response.json(data);
     });
 };
--- public/assests/Todo_List.js
+++ public/assests/Todo_List.js
@@ -1,36 +1,32 @@
 $(document).ready(function(){
 
-    $('form').on('submit', function(){
-  
-        var item = $('form input');
-        var todo = {item: item.val()};
-  
-        $.ajax({
-          type: 'POST',
-          url: '/todo',
-          data: 'form input='+todo,
-          datatype: 'text',
-          success: function(data){
-            //do something with the data via front-end framework
-            alert("Yay!")
-            location.reload();
-          }
-        });
-  
-        return false;
-  
-    });
-  
-    $('li').on('click', function(){
-        var item = $(this).text().replace(/ /g, "-");
-        $.ajax({
-          type: 'DELETE',
-          url: '/todo/' + item,
-          success: function(data){
-            //do something with the data via front-end framework
-            location.reload();
-          }
-        });
-    });
-  
+  $('form').on('submit', function(){
+    
+    var item = $('form input');
+    var todo = {item: item.val()};
+      
+    $.ajax({
+      type: 'POST',
+      url: '/todo',
+      data: todo,
+      success: function(data){
+        //do something with the data via front-end framework
+        location.reload();
+        }
+      });
+    return false;
   });
+  
+  $('li').on('click', function(){
+    var item = $(this).text().replace(/ /g, "-");
+    console.log("Hello from click")
+    $.ajax({
+      type: 'DELETE',
+      url: '/todo/' + item,
+      success: function(data){
+        location.reload();
+        console.log("Hello from Delete Success")
+      }
+    });
+  });
+});
