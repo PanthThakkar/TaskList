0e503345e82dea6fd1dd9d01836267bae45ec066
PanthThakkar <30841919+PanthThakkar@users.noreply.github.com>
2020-06-08 11:48:23 -0400

    Having a dumby database

--- Controllers/ToDoController.js
+++ Controllers/ToDoController.js
@@ -1,22 +1,42 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var data = [{item: 'Get Milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+//To Connect to the database use line 6:
+//Also replace url with the copied cloud database
+//mongoose.connect('<url>');
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+//var data = [{item: 'Get Milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo', function(request, response){
-        response.render('todo', {todos: data});
+        Todo.find({}, function (err, data){
+            if(err){
+                throw err;
+            }
+            response.render('todo', {todos: data});
+        });
     });
     app.post('/todo', urlEncodedParser, function(request, response){
-        data.push(request.body);
-        response.json(data);
-
+        var newToDo = Todo(request.body).save(function (err, data){
+            if(err){
+                throw err;
+            }
+            response.json(data);
+        });
     });
     app.delete('/todo/:item', function(request, response){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== request.params.item;
+        Todo.find({item: request.params.item.replace(/\-/g, " ")}).remove(function (err, data){
+            if(err){
+                throw err;
+            }
+            response.json(data);
         });
-        response.json(data);
     });
 };
